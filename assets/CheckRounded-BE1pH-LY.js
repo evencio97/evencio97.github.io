e62f564bc9e912e40ad1b421e8bedc2852d79d73
evencio <dev.evenciohernandez@gmail.com>
2024-11-08 00:46:44 -0400

    init commit

---
+++ assets/CheckRounded-BE1pH-LY.js
@@ -0,0 +1,1 @@
+import{f as c,j as o}from"./index-WlQsg02S.js";const a=c(o.jsx("path",{d:"M9 16.17 5.53 12.7a.996.996 0 0 0-1.41 0c-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41a.996.996 0 0 0-1.41 0z"}),"CheckRounded");export{a as C};
